f520ebb6f65bba8983bf22b54ef1af67be2a49f2
kamlu <kamlu@kamlesh>
2018-10-27 14:45:41 +0530

    Optimized code

---
+++ MultiThreading/src/org/kp/multithreading/ThreadAlternateSequenceGeneric.java
@@ -0,0 +1,63 @@
+package org.kp.multithreading;
+
+public class ThreadAlternateSequenceGeneric {
+
+	public static void main(String[] args) {
+		ResourceLockG lock = new ResourceLockG();
+		ThreadG.noOfThreads=4;
+		ThreadG a=new ThreadG(lock,1);
+		ThreadG b=new ThreadG(lock,2);
+		ThreadG c=new ThreadG(lock,3);
+		ThreadG d=new ThreadG(lock,4);
+        a.start();
+        b.start();
+        c.start();
+        d.start();
+	}
+
+}
+
+class ResourceLockG{
+    public volatile int flag = 1;
+}
+
+ class ThreadG extends Thread{ 
+	 ResourceLockG lock;
+     int threadNumber;
+     static int noOfThreads;
+     
+    ThreadG(ResourceLockG lock,int threadNumber){
+        this.lock = lock;
+        this.threadNumber=threadNumber;
+    }
+    
+    @Override
+    public void run() {
+        try{
+            synchronized (lock) {
+            	while(true) {
+                    while(lock.flag!=this.threadNumber){
+                        lock.wait();
+                    }
+                    System.out.print(this.threadNumber+" ");
+                    Thread.sleep(1000);
+                    
+                    if(lock.flag==ThreadG.noOfThreads) {
+                    	lock.flag=1;
+                    }
+                    else {
+                    	 lock.flag = this.threadNumber+1;
+                    }
+                    
+                    lock.notifyAll();
+                }
+            }
+        }catch (Exception e) {
+            System.out.println("Exception :"+e.getMessage());
+        }
+    }
+}
+
+ 
+ 
+ 
