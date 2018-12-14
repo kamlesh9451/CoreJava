79da48f6dab7ca726c19412d27002b79e845e42e
kamlu <kamlu@kamlesh>
2018-12-14 19:21:50 +0530

    New calss added

--- CollectionFrameworkExample/src/org/kp/CountTheOccuranceOfeachChrInString.java
+++ CollectionFrameworkExample/src/org/kp/CountTheOccuranceOfeachChrInString.java
@@ -1,10 +1,32 @@
 package org.kp;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
 public class CountTheOccuranceOfeachChrInString {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		String str ="abcdabcdjrfhghjjjjjjjjjjjj";
+		char[] charArray = str.toCharArray();
+		System.out.println(charArray);
+		Set<Character> keySet = null;
+		
+		Map<Character,Integer> map = new HashMap<Character,Integer>();
+		for(char ch :charArray) {
+			keySet = map.keySet();
+			if(keySet.contains(ch)) {
+				map.put(ch, (map.get(ch)+1));
+			}
+			else 
+			{
+				map.put(ch,1);
+			}
+			
+		}
+		
+		System.out.println(map);		
 	}
 
 }
